716df46e7d67b5660b656c11dafb11845d812633
23david18 <pda23whitehat@gmail.com>
2025-10-09 12:31:31 -0500

    Configurando admin.py

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']   # filtros laterales
+    search_fields = ['title', 'body']                          # barra de búsqueda
+    prepopulated_fields = {'slug': ('title',)}                 # autocompleta slug
+    raw_id_fields = ['author']                                 # selector de autor optimizado
+    date_hierarchy = 'publish'                                 # navegación por fechas
+    ordering = ['status', 'publish']                           # orden inicial
