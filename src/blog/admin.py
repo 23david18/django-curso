de3e2da562bb1dc42284a95dc9a395a50cd7da1b
23david18 <pda23whitehat@gmail.com>
2025-10-21 21:43:30 -0500

    Update forms

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -10,3 +10,11 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author']                                 # selector de autor optimizado
     date_hierarchy = 'publish'                                 # navegación por fechas
     ordering = ['status', 'publish']                           # orden inicial
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'update']   
+    search_fields = ['name', 'email', 'body']                          
+   
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+
+from django.views.decorators.http import require_POST
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -35,9 +37,17 @@ def post_detail(request, year, month, day, slug):
                              publish__day=day,
                              slug=slug,
                              status=Post.Status.PUBLISHED)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'form': form
+    }
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  context)
 
 
 def post_share(request, post_id):
@@ -62,3 +72,26 @@ def post_share(request, post_id):
         'sent': sent
     }
     return render(request, 'blog/post/share.html', context)
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                            id=post_id,
+                            status=Post.Status.PUBLISHED)
+    comment = None
+
+    
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    
+    context = {
+        'post': post,
+        'form': form,
+        'comment': comment
+    }
+    return render(request, 'blog/post/comment.html', context)
+        
